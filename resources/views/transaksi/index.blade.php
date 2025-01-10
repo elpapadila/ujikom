812345afe077f4636feef8d927dfbd8e3cee0b6a
elpa padila <elpapadila@gmail.com>
2025-01-10 21:54:40 +0700

    Ubah responsif

--- resources/views/transaksi/index.blade.php
+++ resources/views/transaksi/index.blade.php
@@ -13,8 +13,7 @@
             <h6 class="m-0 font-weight-bold text-primary">Data Transaksi</h6>
         </div>
         <div class="card-body">
-            <div class="table-responsive">
-
+            <div class="table-responsive" style="overflow-x: auto;">
                 <table class="table table-bordered" id="myTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
